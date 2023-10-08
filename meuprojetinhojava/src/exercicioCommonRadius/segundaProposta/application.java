a760e84181b651af10d617c58ba1caf6ccca32e9
vinicius <viniciuscicone@gmail.com>
2023-10-08 11:49:30 -0300

    Adding radius exercise

---
+++ meuprojetinhojava/src/exercicioCommonRadius/segundaProposta/application.java
@@ -0,0 +1,30 @@
+package exercicioCommonRadius.segundaProposta;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class application {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter radius: ");
+		
+		double radius = sc.nextDouble();
+		
+		//Acessando o metodo sem instanciar um objeto. 
+
+		double c = Calculator.circumference(radius);
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
+		
+		sc.close();
+
+	}
+
+}
