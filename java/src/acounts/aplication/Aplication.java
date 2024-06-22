1dbc647529afc9678490cf78edfa47497a2b5561
vinicius <viniciuscicone@gmail.com>
2024-06-22 19:12:46 -0300

    update

---
+++ java/src/acounts/aplication/Aplication.java
@@ -0,0 +1,19 @@
+
+package acounts.aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+import acounts.entities.Accounts;
+import acounts.entities.BusinessAccount;
+import acounts.entities.SavingsAccount;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+
+    }
+}
